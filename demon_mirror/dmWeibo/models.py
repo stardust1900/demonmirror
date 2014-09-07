2f60df792e2fb609949a627ab652378d684ca664
stardust1900 <shawnwang1984@gmail.com>
2014-09-07 22:14:20 +0800

    增加photo模型类

--- demon_mirror/dmWeibo/models.py
+++ demon_mirror/dmWeibo/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,13 @@ class DemonMirror(models.Model):
 	uid = models.CharField(max_length=140,primary_key=True)
 	access_token = models.CharField(max_length=140)
 	expires_in = models.CharField(max_length=140)
+
+# 
+class Photo(models.Model):
+	text = models.CharField(max_length=140)
+	thumbnail_pic = models.CharField(max_length=140)
+	original_pic = models.CharField(max_length=140)
+	post_by = models.CharField(max_length=140)
+	retweet_by = models.CharField(max_length=140)
+	source = models.CharField(max_length=140) # where this photo from weibo douban fanfou this site
+
