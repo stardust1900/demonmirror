ac8d0fae21988a8b27c7cbbee618a0af0d7a4ce4
shawn <stardust1900@hotmail.com>
2014-09-09 17:41:07 +0800

    增加模型

--- demon_mirror/dmWeibo/models.py
+++ demon_mirror/dmWeibo/models.py
@@ -15,4 +15,19 @@ class Photo(models.Model):
 	post_by = models.CharField(max_length=140)
 	retweet_by = models.CharField(max_length=140)
 	source = models.CharField(max_length=140) # where this photo from weibo douban fanfou this site
+	is_show = models.SmallIntegerField() #0:不展示，1：展示
+	status = models.SmallIntegerField()  #0：待审核 1：审核通过  2：发布者删除
+	mark = models.DecimalField(max_digits=4, decimal_places=1) #图片得分
+	marked_num = models.IntegerField()  #评分人数
 
+#评分对象
+class Mark(models.Model):
+	photo = models.ForeignKey(Photo)
+	marker = models.CharField(max_length=140) #打分人
+	marker_from = models.CharField(max_length=140)#打分人登录方式
+	mark = models.SmallIntegerField() #评分
+
+class Comment(models.Model):
+	photo = models.ForeignKey(Photo)
+	text = models.CharField(max_length=140)
+	post_by = models.CharField(max_length=140)
