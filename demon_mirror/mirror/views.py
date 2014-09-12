ccac3683feac7088ff9909ebcbb4f25f04c44126
shawn <stardust1900@hotmail.com>
2014-09-12 16:47:01 +0800

    增加分页

--- demon_mirror/mirror/views.py
+++ demon_mirror/mirror/views.py
@@ -1,15 +1,32 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render_to_response
 from dmWeibo.models import Photo
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+
+
 def showpics(request):
 	photos = Photo.objects.filter(is_show=1).filter(status=1)
-	return render_to_response('showpics.html',{'photos':photos}) 
+	return render_to_response('showpics.html', {'photos': photos})
+
 
 def review(request):
-	photos = Photo.objects.all()[0:10]
+	limit = 20  # 每页显示的记录数
+	photos = Photo.objects.all()
+	paginator = Paginator(photos, limit)  # 实例化一个分页对象
+	page = request.GET.get('page')  # 获取页码
+	try:
+		photos = paginator.page(page)  # 获取某页对应的记录
+	except PageNotAnInteger:  # 如果页码不是个整数
+		photos = paginator.page(1)  # 取第一页的记录
+	except EmptyPage:  # 如果页码太大，没有相应的记录
+		photos = paginator.page(paginator.num_pages)  # 取最后一页的记录
+
 	return render_to_response('review.html',{'photos':photos}) 
 
 def display(request,photoId):
