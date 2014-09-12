32b8f29b2528095853b763ca36a2164b0b95d988
shawn <stardust1900@hotmail.com>
2014-09-12 16:18:33 +0800

    增加审核与展示功能

--- demon_mirror/mirror/urls.py
+++ demon_mirror/mirror/urls.py
@@ -4,4 +4,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('mirror.views',
     url(r'^showpics/$', 'showpics', name='showpics'),
     url(r'^review/$', 'review', name='review'),
+    url(r'^display/(\w+)/$', 'display', name='display'),
+    url(r'^approve/(\w+)/$', 'approve', name='approve'),
+    url(r'^remove/(\w+)/$', 'remove', name='remove'),
 )
--- demon_mirror/mirror/views.py
+++ demon_mirror/mirror/views.py
@@ -1,11 +1,34 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from dmWeibo.models import Photo
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def showpics(request):
-	photos = Photo.objects.all()[0:10]
+	photos = Photo.objects.filter(is_show=1).filter(status=1)
 	return render_to_response('showpics.html',{'photos':photos}) 
 
 def review(request):
 	photos = Photo.objects.all()[0:10]
 	return render_to_response('review.html',{'photos':photos}) 
+
+def display(request,photoId):
+	photo = Photo.objects.get(id=photoId)
+	if 0==photo.is_show:
+		photo.is_show = 1
+	else:
+		photo.is_show = 0
+	photo.save()
+	return HttpResponseRedirect(reverse('mirror.views.review'))
+
+def approve(request,photoId):
+	photo = Photo.objects.get(id=photoId)
+	photo.status = 1
+	photo.is_show = 1
+	photo.save()
+	return HttpResponseRedirect(reverse('mirror.views.review'))
+
+def remove(request,photoId):
+	photo = Photo.objects.get(id=photoId)
+	photo.delete()
+	return HttpResponseRedirect(reverse('mirror.views.review'))
