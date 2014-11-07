c4d90f32e1b6d18e322aa781ce13009048ec1633
shawn <stardust1900@hotmail.com>
2014-11-07 17:50:25 +0800

    修改导航

--- sae/1/demonmirror/urls.py
+++ sae/1/demonmirror/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
 admin.autodiscover()
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -24,5 +25,8 @@ urlpatterns = patterns('',
     url(r'^get_mentions$', 'dmWeibo.views.get_mentions', name='get_mentions'),
     url(r'^test','dmWeibo.views.test',name='test'),
     url(r'^mirror/', include('mirror.urls')),
+    url(r'^rater/', include('rater.urls')),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',{'template_name':'login.html'}),
+    url(r'^$', RedirectView.as_view(url='/mirror/showpics/')),
 )
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
